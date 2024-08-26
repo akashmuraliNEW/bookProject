e47d9bb67bdde288883e22052383c7ba23617c79
AKASH KM <akashunni869@gmail.com>
2024-08-26 21:05:54 +0530

    first commit

---
+++ bookApp/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Author(models.Model):
+    author = models.CharField(max_length=100)
+    def __str__(self):
+        return self.author
+    
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    price = models.IntegerField(max_length=100)
+    image = models.ImageField(default='default_image.png', upload_to='book_images')
+    def __str__(self):
+        return self.title
---
+++ bookApp/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render,redirect
+from .models import *
+# Create your views here.
+
+
+
+def homePage(request):
+    author = Author.objects.all()
+    # print('creating ..')
+    if request.method =='POST':
+        title = request.POST['title']
+        price = request.POST['price']
+        image = request.FILES['image']
+        author_name = request.POST['author']
+        author_obj = Author.objects.get(author=author_name)
+        obj = Book.objects.create(title=title, price=price, author=author_obj,image=image)
+        obj.save()
+        # print('done../')
+        return redirect('display')
+    return render(request, 'home.html', {'author': author})
+
+
+def display_books(request):
+    books = Book.objects.all()
+    return render(request,'display_books.html',{'book':books})
+
+def update_books(request,id):
+    author = Author.objects.all()
+    book = Book.objects.get(id=id)
+    # print(book)
+    if request.method == 'POST':
+        book.title = request.POST['title']
+        book.image = request.POST['image']
+        print(book.title)
+        book.price = request.POST['price']
+        author_name = request.POST['author']
+        author_obj = Author.objects.get(author=author_name)
+        book.author = author_obj
+        book.save()
+        return redirect('display')
+    return render(request, 'update_book.html', {'book': book, 'author': author})
+
+
+def delete_book(request,id):
+    author = Author.objects.all()
+    book = Book.objects.get(id=id)
+    book.delete()
+    
+    return render(request,'delete_book.html', {'book':book,'author':author})
+    
